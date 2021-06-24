82202c459703103ec3faf4c04850978b0850736a
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-06-24 23:47:13 +0200

    Storybook for addons

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import '@plone/volto/config';
 
 export default function applyConfig(config) {
+  config.views.contentTypesViews.knowledge = (props) => "I'm a component";
   return config;
 }
