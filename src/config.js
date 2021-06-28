ffa4fda27fe441b27192359974a8a4bd82ac917f
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-06-28 17:05:34 +0200

    Remove dangling configg

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-import React from 'react';
 import '@plone/volto/config';
 
 export default function applyConfig(config) {
-  config.views.contentTypesViews.knowledge = (props) => "I'm a component";
   return config;
 }
